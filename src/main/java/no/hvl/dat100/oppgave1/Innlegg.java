4f1689a4c16d41d02824047353efdd167c5f95e5
Duy Hoang Vu <duyhoang@hotmail.no>
2025-11-02 02:40:28 +0100

    Update Innlegg.java

--- src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
+++ src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
@@ -5,22 +5,28 @@ import no.hvl.dat100.common.TODO;
 public abstract class Innlegg {
 	
 	// TODO - deklarering av objektvariable
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
+
 	
 	public Innlegg() {
 		
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = 0;
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
-
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = likes;
 	}
 	
 	public String getBruker() {
